986a173a38f4076f0c55d2c1c3455016da9e0da6
ArnurSakenov <arnur.s@mail.ru>
2023-05-10 22:10:42 +0600

    table

--- JusanBooking/RoomDetail/RoomDetailsTableViewCell.swift
+++ JusanBooking/RoomDetail/RoomDetailsTableViewCell.swift
@@ -8,6 +8,121 @@
 import UIKit
 
 class RoomDetailsTableViewCell: UITableViewCell {
-    static let identifier = "roomDetailsCell"
-
+    static let identifier = "reservationtableviewcell"
+    
+    private let dayOfWeekLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.borderColor = UIColor.orange.cgColor
+        view.layer.borderWidth = 1.0
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let exactDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .orange
+        return label
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    
+    private let eventTypeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.orange
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubviews()
+        setupConstraints()
+        
+        contentView.backgroundColor = .clear
+        containerView.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubviews() {
+        
+        containerView.addSubview(dayOfWeekLabel)
+        containerView.addSubview(exactDateLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(eventTypeLabel)
+        containerView.addSubview(separatorView)
+        contentView.addSubview(containerView)
+    }
+    
+    private func setupConstraints() {
+        containerView.snp.makeConstraints { (make) in
+            make.edges.equalTo(contentView).inset(10)
+        }
+        dayOfWeekLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(containerView.snp.top).offset(16)
+            make.leading.equalTo(containerView.snp.leading).offset(16)
+            make.trailing.lessThanOrEqualTo(containerView.snp.centerX)
+        }
+        
+        exactDateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(containerView.snp.top).offset(16)
+            make.leading.greaterThanOrEqualTo(containerView.snp.centerX)
+            make.trailing.equalTo(containerView.snp.trailing).offset(-16)
+        }
+        
+        separatorView.snp.makeConstraints { (make) in
+            make.top.equalTo(dayOfWeekLabel.snp.bottom).offset(8)
+            make.leading.equalTo(containerView.snp.leading).offset(16)
+            make.trailing.equalTo(containerView.snp.trailing).offset(-16)
+            make.height.equalTo(1)
+        }
+        
+        timeLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(separatorView.snp.bottom).offset(16)
+            make.leading.equalTo(containerView.snp.leading).offset(16)
+            make.trailing.lessThanOrEqualTo(containerView.snp.centerX)
+            make.bottom.equalTo(containerView.snp.bottom).offset(-16)
+        }
+        
+        eventTypeLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(separatorView.snp.bottom).offset(16)
+            make.leading.greaterThanOrEqualTo(containerView.snp.centerX)
+            make.trailing.equalTo(containerView.snp.trailing).offset(-16)
+            make.bottom.equalTo(containerView.snp.bottom).offset(-16)
+        }
+    }
+    
+    func configure(dayOfWeek: String, exactDate: String, time: String, eventType: String) {
+        dayOfWeekLabel.text = dayOfWeek
+        exactDateLabel.text = exactDate
+        timeLabel.text = time
+        eventTypeLabel.text = eventType
+    }
 }
+
--- JusanBooking/RoomDetail/RoomDetailslViewController.swift
+++ JusanBooking/RoomDetail/RoomDetailslViewController.swift
@@ -13,34 +13,60 @@ class RoomDetailslViewController: UIViewController {
         super.viewDidLoad()
         setSubviews()
         setConstraints()
+        view.backgroundColor = #colorLiteral(red: 0.06831727177, green: 0.09892369062, blue: 0.1742413342, alpha: 1)
         roomImageView.image = UIImage(named: "coworking1")
-        roomNumberLabel.text = "Room 1"
-        capacityLabel.text = "Capacity: 1"
-        floorLabel.text = "Floor: 1"
+     
+      
+        
+        
       
     }
+    private let bookingButton: UIButton = {
+        let bookingButton = UIButton()
+        bookingButton.setTitle("BOOK THIS ROOM", for: .normal)
+        bookingButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
+        bookingButton.setTitleColor(.white, for: .normal)
+        bookingButton.backgroundColor = #colorLiteral(red: 0.9191874266, green: 0.3177170753, blue: 0.1384931207, alpha: 1)
+        bookingButton.layer.cornerRadius = 8
+//        bookingButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        return bookingButton
+    }()
     private let reservationTableView: UITableView  = {
         let table = UITableView()
         table.backgroundColor = #colorLiteral(red: 0.06831727177, green: 0.09892369062, blue: 0.1742413342, alpha: 1)
+
         table.register(RoomDetailsTableViewCell.self, forCellReuseIdentifier: RoomDetailsTableViewCell.identifier)
         return table
     }()
     private func setSubviews(){
         view.addSubview(reservationTableView)
         view.addSubview(roomImageView)
+        detailsStackView.addArrangedSubview(roomCategoryLabel)
         detailsStackView.addArrangedSubview(roomNumberLabel)
+        
         detailsStackView.addArrangedSubview(capacityLabel)
         detailsStackView.addArrangedSubview(floorLabel)
-        view.addSubview(detailsStackView)
+        containerView.addSubview(detailsStackView)
+        view.addSubview(containerView)
+        view.addSubview(bookingButton)
         reservationTableView.delegate = self
         reservationTableView.dataSource = self
         
     }
+    private let containerView: UIView = {
+           let view = UIView()
+           view.backgroundColor = .white
+           view.layer.borderColor = UIColor.orange.cgColor
+           view.layer.borderWidth = 1.0
+           view.layer.cornerRadius = 10
+           view.translatesAutoresizingMaskIntoConstraints = false
+           return view
+       }()
     private let detailsStackView: UIStackView = {
         let stack = UIStackView()
-        stack.distribution = .equalSpacing
+        stack.distribution = .fillProportionally
         stack.axis = .vertical
-        stack.backgroundColor = .systemBlue
+        stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
     private let roomImageView: UIImageView = {
@@ -56,38 +82,74 @@ class RoomDetailslViewController: UIViewController {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .orange
+        label.text = "Room:"
+        return label
+    }()
+    
+    
+    private let roomCategoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .orange
+        label.text = "Category:"
         return label
     }()
     
     private let capacityLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .orange
+        label.text = "Capacity:"
         return label
     }()
     
     private let floorLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .orange
+        label.text = "Floor:"
         return label
     }()
+    private let headerLabel: UILabel = {
+           let label = UILabel()
+           label.text = "Reservations"
+           label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+           label.textAlignment = .center
+           label.textColor = .white
+           return label
+       }()
     private func setConstraints(){
         
         roomImageView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.equalTo(view.safeAreaLayoutGuide)
-            make.width.equalTo(200)
+            make.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(250)
+        }
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(roomImageView.snp.bottom).offset(10)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-10)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
             make.height.equalTo(150)
+        }
+        bookingButton.snp.makeConstraints { make in
+            make.top.equalTo(containerView.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(50)
         }
         detailsStackView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.trailing.equalTo(view.safeAreaLayoutGuide)
-            make.width.equalTo(200)
-            make.height.equalTo(150)
+            make.top.equalTo(containerView.snp.top)
+            make.leading.equalTo(containerView.snp.leading).offset(10)
+            make.width.equalTo(containerView.snp.width).multipliedBy(0.5)
+            make.bottom.equalTo(containerView.snp.bottom)
         }
         reservationTableView.snp.makeConstraints { make in
-            make.top.equalTo(roomImageView.snp.bottom).offset(20)
+            make.top.equalTo(bookingButton.snp.bottom).offset(20)
             make.bottom.trailing.leading.equalTo(view.safeAreaLayoutGuide)
         }
         
@@ -103,12 +165,18 @@ extension RoomDetailslViewController: UITableViewDelegate, UITableViewDataSource
         guard  let cell = tableView.dequeueReusableCell(withIdentifier: RoomDetailsTableViewCell.identifier, for: indexPath) as? RoomDetailsTableViewCell else {return UITableViewCell()}
         
         cell.backgroundColor = #colorLiteral(red: 0.06831727177, green: 0.09892369062, blue: 0.1742413342, alpha: 1)
+        cell.configure(dayOfWeek: "Monday", exactDate: "19-10-2023", time: "12:30-13:00", eventType: "Lecture")
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 250
+        return 120
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        navigationController?.pushViewController(RoomDetailslViewController(), animated: true)
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        return headerLabel
+//    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Reservations"
     }
+   
 }
